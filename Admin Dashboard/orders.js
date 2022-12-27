f9ac577edc3d39e179e4567a2a7f23c74ee45e0d
miraclemarcel <miraclemarcel29@gmail.com>
2022-12-27 14:30:19 +0100

    First commit

---
+++ Admin Dashboard/orders.js
@@ -0,0 +1,56 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '68890',
+        paymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+    {
+        productName: '42 Inches LG smart led TV',
+        productNumber: '23451',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'White Nike Sneakers',
+        productNumber: '65321',
+        paymentStatus: 'Declined',
+        Shipping: 'Cancelled'
+    },
+    {
+        productName: 'Samsung Galaxy A13, 6.6", 4GB/64GB',
+        productNumber: '68675',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: '2 In 1 Men\'s Short Sleeve Shorts Casual Set-White',
+        productNumber: '23490',
+        paymentStatus: 'Paid',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '68890',
+        paymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+    {
+        productName: '42 Inches LG smart led TV',
+        productNumber: '23451',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'White Nike Sneakers',
+        productNumber: '65321',
+        paymentStatus: 'Declined',
+        Shipping: 'Cancelled'
+    },
+    
+
+
+
+
+
+]
